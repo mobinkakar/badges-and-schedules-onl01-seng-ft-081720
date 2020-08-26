6e3f089ae5abf766168c042c7488cfd3e19b4511
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-08-26 16:34:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,9 +12,11 @@ return array
 end
 
 def assign_rooms(room)
+  room = 0
   array = []
   room.each do|room|
     array << "Hello #{name}! You'll be assigned to room #{room}"
+    room += 1
   end
   return array
 end
